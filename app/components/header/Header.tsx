09681c00ab877e4facc7df9671966b3f90457874
Oskar Bergman <ocke_bergman@hotmail.com>
2025-01-25 21:55:06 +0100

    fixed issue with notes not being cleared on logout

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,39 +1,30 @@
 import AddButton from '@/app/components/header/AddButton';
-import { auth, signOut } from '@/auth';
+import {auth} from '@/auth';
 import Image from 'next/image';
 import Link from 'next/link';
-import LogoutButton from '@/app/components/header/LogoutButton';
 import LoginButton from '@/app/components/header/LoginButton';
-
-function SignOut() {
-    return (
-        <form action={async () => {
-            'use server';
-            await signOut()
-        }}>
-        <LogoutButton/>
-        </form>
-    )
-}
-
+import LogoutButton from '@/app/components/header/LogoutButton';
 
 const Header = async () => {
     const session = await auth();
-    console.log("session header: ", session);
+    console.log('session header: ', session);
     return (
         <header className="flex flex-col items-center border-b p-4">
             <nav className="flex items-center justify-between w-full">
                 {session?.user ? (
                     <div className="flex-col space-y-2">
-                        {session.user.name && <span className="text-gray-200">{session.user.name}</span>}
+                        {session.user.name && <span
+                            className="text-gray-200">{session.user.name}</span>}
                         {session.user.image &&
-                            <Image src={session.user.image} alt={session.user.name ?? "User"} width={32} height={32}
+                            <Image src={session.user.image}
+                                   alt={session.user.name ?? 'User'} width={32}
+                                   height={32}
                                    className="rounded-full"/>}
-                        <SignOut/>
+                        <LogoutButton />
                     </div>
                 ) : (
                     <Link href="/login">
-                        <LoginButton />
+                        <LoginButton/>
                     </Link>
                 )
                 }
